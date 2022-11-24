a5ccd8e309ba3ca9d9d6842a19f65e7100acce20
Kemal <i.kemal.karabulut@gmail.com>
2022-11-24 11:43:46 +0100

    Login variables fixed

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,17 +1,41 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.REACT_APP_apiKey,
-  authDomain: process.REACT_APP_authDomain,
-  projectId: process.REACT_APP_projectId,
-  storageBucket: process.REACT_APP_storageBucket,
-  messagingSenderId: process.REACT_APP_messagingSenderId,
-  appId: process.REACT_APP_appId,
-};
+console.log(import.meta.env.VITE_apiKey);
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD3lzS3q3XY1hC42vtS9DNpq9aafDiO_Zo",
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export async function SignUp(email, password) {
+  try {
+    const user = await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function LoginUser(email, password) {
+  try {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    console.log(user);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../Styling/Container.styled";
 import LoginContainer, { HeaderLogo, Logo } from "../Styling/Login.styled";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { LoginUser } from "../helpers/firebase";
 
 const Login = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUser((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+  function handleLogin() {
+    console.log("Clicked on login");
+    try {
+      LoginUser(user.email, user.password);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  console.log(user);
   return (
     <Container>
       <LoginContainer>
@@ -13,16 +34,26 @@ const Login = () => {
         <TextField
           id="outlined-basic"
           label="Email"
+          name="email"
+          onChange={handleChange}
           variant="outlined"
+          value={user.email || ""}
           sx={{ width: "80%", marginBottom: "30px" }}
         />
         <TextField
           id="outlined-basic"
           label="Password"
+          onChange={handleChange}
+          name="password"
           variant="outlined"
+          value={user.password || ""}
           sx={{ width: "80%", marginBottom: "30px" }}
         />
-        <Button variant="contained" sx={{ display: "block", minWidth: "90px" }}>
+        <Button
+          variant="contained"
+          sx={{ display: "block", minWidth: "90px" }}
+          onClick={handleLogin}
+        >
           Login
         </Button>
       </LoginContainer>
