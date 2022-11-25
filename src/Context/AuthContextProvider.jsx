1dc9874e90a27ebcafa43cb8e09d6127e1551550
Kemal <i.kemal.karabulut@gmail.com>
2022-11-25 12:15:57 +0100

    message added contextprovider

--- src/Context/AuthContextProvider.jsx
+++ src/Context/AuthContextProvider.jsx
@@ -4,6 +4,10 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  // When this was only set to null, on every refresh, the user was initially set to null and this
+  // direct the user to login page. Even though in 1 sec later user seem logged in. Leaving it as an empty
+  // object fixed the issue.
+
   console.log(user);
   return (
     <AuthContext.Provider value={{ user, setUser }}>
