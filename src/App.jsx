b82c5186261a94a11c61aebf8d0dc6c7efd1a561
Kemal <i.kemal.karabulut@gmail.com>
2022-12-07 13:41:14 +0100

    config

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import AppRouter from "./Router/AppRouter";
 import AuthContextProvider from "./Context/AuthContextProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import * as dotenv from "dotenv";
 
 function App() {
+  // console.log(process.env);
   return (
     <AuthContextProvider>
       <ToastContainer />
